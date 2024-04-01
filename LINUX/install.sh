7faa84a9f36aebbb5b6fd7b7dcbcc0f18d9b4a87
richneese <r.neese@gmail.com>
2024-04-01 12:47:54 -0400

    new install script

--- LINUX/install.sh
+++ LINUX/install.sh
@@ -1,18 +1,23 @@
 #!/bin/sh
 
-# Dir for building pkgs
-mkdir tmp
-
-#####################################################
-#
-# prepeare your Linux system for compiling piHPSDR
-#
-######################################################
-echo "First we need to make sure dependancies are installed."
-
-echo "There may be several packages to install.  This could"
-
-echo "take several minutes to complete."
+echo "#################################################################"
+echo ""
+echo " Prepeare your Linux system for compiling and Installing "
+echo ""
+echo "                       piHPSDR "
+echo""
+echo "#################################################################"
+echo ""
+echo "################################################################"
+echo ""
+echo " First we need to make sure dependancies are installed. "
+echo ""
+echo " There may be several packages to install.  "
+echo ""
+echo " This could take several minutes to complete. "
+echo ""
+echo "################################################################"
+echo ""
 
 ################################################################
 #
@@ -86,6 +91,7 @@ echo "Installing AIRSPYHF"
 sudo apt -y install airspyhf
 sudo apt -y install libairspyhf-dev
 echo "Done"
+echo ""
 
 #########################################################
 # Lib for hackers
@@ -95,6 +101,7 @@ echo "Installing HackRF"
 sudo apt -y install hackrf
 sudo apt -y install libhackrf-dev
 echo "Done"
+echo ""
 
 #########################################################
 # Limes suite
@@ -103,6 +110,7 @@ echo "Installing LimeSuite"
 sudo apt -y install limesuite
 sudo apt -y install liblimesuite-dev
 echo "Done"
+echo ""
 
 ################################################################
 #
@@ -115,15 +123,21 @@ echo "Installing SoapySDR and Deps"
 sudo apt -y install libsoapysdr-dev
 sudo apt -y install soapysdr-module-all
 sudo apt -y install soapysdr-module-xtrx 
-sudo apt -y install soapysapt-cache search bladerf-*dr-tools
+sudo apt -y install soapysapt-cache
 sudo apt -y install libsoapysdr-doc
 sudo apt -y install uhd-host
 sudo apt -y install uhd-soapysdr
 sudo apt -y install soapysdr-module-uhd
 echo "Done"
+echo ""
 
 ################################################################
-# GIt the sdrplay soapy audio module and build it.
+# Dir for building pkgs
+################################################################
+mkdir tmp
+
+################################################################
+# GIt the sdrplay soapy audio module and building & install it.
 ################################################################
 echo " building and installing SoapyAudio"
 cd tmp
@@ -136,9 +150,10 @@ make
 sudo make install
 cd ~
 echo "Done"
+echo ""
 
 ################################################################
-# GIt the sdrplay SoapyRTLTCP module and build it.
+# GIt the sdrplay SoapyRTLTCP module and building  & install it.
 ################################################################
 echo " building and installing SoapyRTLTCP"
 cd tmp
@@ -151,9 +166,10 @@ make
 sudo make install
 cd ~
 echo "Done"
+echo ""
 
 ################################################################
-# GIt the sdrplay soapyPlutoSDR module and build it.
+# GIt the sdrplay soapyPlutoSDR module and building & install it.
 ################################################################
 echo " building and installing SoapyPlutoSDR"
 cd tmp
@@ -166,9 +182,10 @@ make
 sudo make install
 cd ~
 echo "Done"
+echo ""
 
 ################################################################
-# GIt the sdrplay soapyPlutoSDR module and build it.
+# GIt the sdrplay soapyPlutoSDR module and building & install it.
 ################################################################
 echo " building and installing SoapyMultiSDR"
 cd tmp
@@ -183,11 +200,11 @@ cd ~
 echo "Done"
 
 ################################################################
-# GIt the sdrplay soapy NestSDR module and build it.
+# GIt the sdrplay soapy NetSDR module and building & install it.
 ################################################################
-echo " building and installing SoapyNestSDR"
+echo " building and installing SoapyNetSDR"
 cd tmp
-git clone https://github.com/pothosware/SoapyNestSDR.git
+git clone https://github.com/pothosware/SoapyNetSDR.git
 cd SoapyNestSDR
 mkdir build
 cd build
@@ -196,64 +213,71 @@ make
 sudo make install
 cd ~
 echo "Done"
+echo ""
 
 ################################################################
-# GIt the sdrplay soapyPlutoSDR module and build it.
+# Modules in the works 
 ################################################################
-echo " building and installing SoapySpyServer"
-git clone https://github.com/pothosware/SoapySpyServer.git
-cd SoapySpyServer
-mkdir build
-cd build
-cmake ..
-make
-sudo make install
-cd ~
-echo "Done"
+################################################################
+# Git the sdrplay soapySpyServer module and buildin & install it.
+################################################################
+#echo " building and installing SoapySpyServer"
+#git clone https://github.com/pothosware/SoapySpyServer.git
+#cd SoapySpyServer
+#mkdir build
+#cd build
+#cmake ..
+#make
+#sudo make install
+#cd ~
+#echo "Done"
 
 ################################################################
-# GIt the sdrplay soapyPlutoSDR module and build it.
+# Git the sdrplay soapyPlutoSDR module and building & install it.
 ################################################################
-echo " building and installing SoapyVOLKConverters"
-git clone https://github.com/pothosware/SoapyVOLKConverters.git
-cd SoapyVOLKConverters
-mkdir build
-cd build
-cmake ..
-make
-sudo make install
-cd ~
-echo "Done"
+#echo " building and installing SoapyVOLKConverters"
+#git clone https://github.com/pothosware/SoapyVOLKConverters.git
+#cd SoapyVOLKConverters
+#mkdir build
+#cd build
+#cmake ..
+#make
+#sudo make install
+#cd ~
+#echo "Done"
+#echo ""
 
 ################################################################
-# GIt the sdrplay soapy sidekiq module and build it.
+# GIt the sdrplay soapy sidekiq module and building & install it.
 ################################################################
-echo " building and installing SoapySidekiq"
-cd tmp
-git clone https://github.com/pothosware/SoapySidekiq.git
-cd SoapySidekiq
-mkdir build
-cd build
-cmake ..
-make
-sudo make install
-cd ~
-echo "Done"
+#echo " building and installing SoapySidekiq"
+#cd tmp
+#git clone https://github.com/pothosware/SoapySidekiq.git
+#cd SoapySidekiq
+#mkdir build
+#cd build
+#cmake ..
+#make
+#sudo make install
+#cd ~
+#echo "Done"
+#echo ""
 
 ################################################################
-# GIt the sdrplay soapy FunCube module and build it.
+# GIt the sdrplay soapy FunCube module and building & install it.
 ################################################################
-echo " building and installing SoapyFCDPP"
-cd tmp
-git clone https://github.com/pothosware/SoapyFCDPP.git
-cd SoapyFCDPP
-mkdir build
-cd build
-cmake ..
-make
-sudo make install
-cd ~
-echo "Done"
+#echo " building and installing SoapyFCDPP"
+#cd tmp
+#git clone https://github.com/pothosware/SoapyFCDPP.git
+#cd SoapyFCDPP
+#mkdir build
+#cd build
+#cmake ..
+#make
+#sudo make install
+#cd ~
+#echo "Done"
+#echo ""
 
 ################################################################
 #
@@ -264,7 +288,9 @@ echo "Installing SDRPLAY API FOR Linux And SOAPYSDR"
 wget https://www.sdrplay.com/software/SDRplay_RSP_API-Linux-3.14.0.run
 sudo chmod 755 ./SDRplay_RSP_API-Linux-3.14.0.run
 sudo ./SDRplay_RSP_API-Linux-3.14.0.run
+cd ~
 echo "Done"
+#echo ""
 
 ################################################################
 # GIt the sdrplay soapy module and build it.
@@ -279,13 +305,66 @@ make
 sudo make install
 cd ~
 echo "Done"
+echo ""
 
+################################################################
+# Install  Wdsp
+################################################################
+echo " Installing wdsp lib "
+cd tmp
+git clone https://github.com/dl1ycf/wdsp.git
+cd wdsp
+make clean
+make -j4
+sudo make install
+cd ~
+echo "Done"
+echo ""
+
+################################################################
+# Remove the tmp dir and cleaning up space
+################################################################
 rm -rf tmp
 
 echo ""
-echo "IF any errors please report them so that they can be fixed"
+echo "################################################################"
+echo " IF any errors please report them so that they can be fixed"
+echo "################################################################"
+echo " If there were no errors reporte."
+echo " Everything should be ready to go."
+echo " You can Now build piHPSDR "
+echo "################################################################"
 echo ""
-echo "If there were no errors reported, please reboot your machine."
-echo "Everything should be ready to go."
-echo "You can Now build SDR++ / SDR++Brown"
+
+echo "#####################################################"
 echo ""
+echo " Building and installing piHPSDR"
+echo""
+echo "#####################################################"
+mkdir tmp
+cd tmp
+git clone -b my-work-branch --depth=1 https://github.com/RichNeese/pihpsdr.git
+cd pihpsdr
+cp Makefile MakefileGNU
+echo "##############################################################"
+echo ""
+echo " Editing MakfileGNU to enable the features you want."
+echo ""
+echo " When finshed editing hit ctl+x to exit and save the changes"
+echo ""
+echo "##############################################################"
+nano MakefileGNU
+echo "buildig piHPSDH/LinHPSDR"
+make -j4 -f MakefileGNU
+echo " Installing piHPSDR / LinHPSDRR"
+make -f MakefileGNU install
+echo " Doing Cleanup"
+make clean
+cd ~
+rm -rf tmp
+
+echo "#####################################################"
+echo ""
+echo " piHPSDR is installed please reboot system "
+echo""
+echo "#####################################################"
